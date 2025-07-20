be83cef08c722a99955f5fa6af7cad9b3ebefac5
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-07-20 08:14:42 -0400

    0.0.1
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- external-checks/Palo Alto Networks GlobalProtect Status.bash
+++ external-checks/Palo Alto Networks GlobalProtect Status.bash
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-#####################################################################################
-#
-# Palo Alto Networks GlobalProtect Status
-# for
-# Setup Your Mac via swiftDialog
-#
-####################################################################################
-#
-# HISTORY
-#
-#   Version 0.0.1, 16-Dec-2022, Dan K. Snelson (@dan-snelson)
-#   - Original Version
-#
 ###########################################################################################
 # A script to collect the status of Palo Alto GlobalProtect.                              #
 # • If Palo Alto GlobalProtect is not installed, "Not Installed" will be returned.        #
 # • If the local user is not logged-in, "${loggedInUser} not logged-in" will be returned. #
 # • If no gateway is selected, "Best Available Gateway selected" will be returned.        #
+###########################################################################################
+#
+# HISTORY
+#
+#   Version 0.0.1, 14-Dec-2022, Dan K. Snelson (@dan-snelson)
+#   - Original Version
+#
 ###########################################################################################
 
 loggedInUser=$( /bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }' )
@@ -40,24 +34,21 @@ if [[ -d /Applications/GlobalProtect.app ]]; then # GlobalProtect.app found in /
     esac
 
     # Read the logged-in user's `User`
-    userResult=$( /usr/bin/defaults read /Users/"${loggedInUser}"/Library/Preferences/com.paloaltonetworks.GlobalProtect.client User 2>&1 )
+    userResult=$( /usr/bin/defaults read /Users/${loggedInUser}/Library/Preferences/com.paloaltonetworks.GlobalProtect.client User 2>&1 )
     if [[ "${userResult}"  == *"Does Not Exist" || -z "${userResult}" ]]; then
         userResult="${loggedInUser} NOT logged-in to GlobalProtect; "
-    # elif [[ ! -z "${userResult}" ]]; then
-    elif [[ -n "${userResult}" ]]; then
-
+    elif [[ ! -z "${userResult}" ]]; then
         userResult="\"${loggedInUser}\" logged-in to GlobalProtect; "
     fi
 
     # Read `Portal`
-    companyPortal=$( /usr/libexec/PlistBuddy -c "print :Palo\ Alto\ Networks:GlobalProtect:PanSetup:Portal" /Library/Preferences/com.paloaltonetworks.GlobalProtect.settings.plist 2>&1 )
+    companyPortal=$( /usr/libexec/PlistBuddy -c "print :Palo\ Alto\ Networks:GlobalProtect:PanSetup:Portal" /Library/Preferences/com.paloaltonetworks.GlobalProtect.settings.plist )
 
     # Read `default-gateway-name`
-    gatewayResult=$( /usr/libexec/PlistBuddy -c "print :${companyPortal}:default-gateway-name" /Users/"${loggedInUser}"/Library/Preferences/com.paloaltonetworks.GlobalProtect.client.plist 2>&1 )
+    gatewayResult=$( /usr/libexec/PlistBuddy -c "print :${companyPortal}:default-gateway-name" /Users/${loggedInUser}/Library/Preferences/com.paloaltonetworks.GlobalProtect.client.plist 2>&1 )
     if [[ "${gatewayResult}" == *"Does Not Exist" ]]; then
         gatewayResult="No default gateway specified"
-    # elif [[ ! -z "${gatewayResult}" ]]; then
-    elif [[ -n "${gatewayResult}" ]]; then
+    elif [[ ! -z "${gatewayResult}" ]]; then
         gatewayResult="\"${loggedInUser}\" selected \"${gatewayResult}\" as default gateway"
     fi
 
@@ -68,4 +59,4 @@ else # GlobalProtect.app NOT found in /Applications; clear other variables
 
 fi
 
-/bin/echo "<result>${globalProtectDisabled}</result>"
+/bin/echo "<result>${appResult}${globalProtectDisabled}${userResult}</result>"
